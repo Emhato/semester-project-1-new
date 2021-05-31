e2295f5b3a97c9fd111d06ed206be0b616e99f1d
Emhato <71319235+Emhato@users.noreply.github.com>
2021-06-01 01:52:28 +0200

    carousel started

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const hamburger = document.querySelector(".fa-bars");
 const cross = document.querySelector(".fa-times");
 const navigation = document.querySelector("nav");
 const navBareMobile = document.querySelector(".top-nav-mobile");
+const logo = document.querySelector(".text-logo");
 
 
 function hamburgerClicked() {
     navigation.style.display = "block";
     hamburger.style.display = "none";
     cross.style.display = "block";
-    navBareMobile.style.backgroundColor = "#FEE6CC";
-    navBareMobile.style.backgroundImage = "none"
+    logo.style.display = "none";
 }
 
 hamburger.addEventListener("click", hamburgerClicked);
@@ -20,7 +20,7 @@ function crossClicked() {
     navigation.style.display = "none";
     hamburger.style.display = "block";
     cross.style.display = "none";
-    navBareMobile.style.backgroundImage = "url(/images/sunrise.jpg)";
+    logo.style.display = "block";
 }
 
 cross.addEventListener("click", crossClicked);
